0e65b991d5e97b03e028023c3e7fc76e0a8730c9
avinash chavan <chavan.avinash83@gmail.com>
2026-06-11 09:15:07 +0000

    Try fixing this error: `Build Error: Export errorEmitter doesn't exist i

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -18,3 +18,4 @@ export * from './client-provider';
 export * from './auth/use-user';
 export * from './firestore/use-doc';
 export * from './firestore/use-collection';
+export * from './error-emitter';
